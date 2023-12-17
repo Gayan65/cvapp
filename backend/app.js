72601db7c90a1b7bcfb4abeb75855a59a3f40067
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-17 14:03:24 +0200

    db connected

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,30 @@
 // IMPORTING PACKAGES
 import express from "express";
+import mysql from "mysql2";
 
 //IMPORTING ENV VARIABLES
 import "dotenv/config";
 
 const port = process.env.PORT;
+const url = process.env.DATABASE_URL;
+const user = process.env.USER;
+const password = process.env.PASSWORD;
 const app = express();
 
 app.listen(port, () => {
   console.log(`Server is running in port ${port} successfully`);
+});
+
+//database connection
+const con = mysql.createConnection(url);
+
+//table creating function
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE datas (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
