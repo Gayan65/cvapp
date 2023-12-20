0fdfc4af273209c8f7da128e73797af50acad959
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-20 15:10:30 +0200

    contact table create delete function add, working

--- backend/controllers/table_controllers.js
+++ backend/controllers/table_controllers.js
@@ -4,6 +4,8 @@ import {
   deleteUserTable,
   createPersonalTable,
   deletePersonalTable,
+  createContactTable,
+  deleteContactTable,
 } from "../services/table_services.js";
 
 const tableRouter = express.Router();
@@ -29,6 +31,18 @@ tableRouter.post("/personal/create", async (req, res) => {
 //deleting personal table
 tableRouter.delete("/personal/delete", async (req, res) => {
   const newTable = await deletePersonalTable();
+  res.send(newTable);
+});
+
+//creating contact table
+tableRouter.post("/contact/create", async (req, res) => {
+  const newTable = await createContactTable();
+  res.send(newTable);
+});
+
+//deleting contact table
+tableRouter.delete("/contact/delete", async (req, res) => {
+  const newTable = await deleteContactTable();
   res.send(newTable);
 });
 export default tableRouter;
