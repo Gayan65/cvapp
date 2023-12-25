84bac3c7981cc66ad70118ff38ebfa73bc9f3d33
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-25 13:54:17 +0200

    basic route structure add, working

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login.js";
 
 function App() {
-  return <div className="fs-1 test">Hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
