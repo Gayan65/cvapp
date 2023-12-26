2155539c55c92d4a8e4a627777e0db3d99835970
Gayan Withana Gamage <gayan65728@yahoo.com>
2023-12-26 14:51:31 +0200

    Google font, button css add, working

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Start from "./Start";
 import Login from "./Login";
 import Register from "./Register";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Start />}></Route>
           <Route path="/login" element={<Login />}></Route>
--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -12,23 +12,25 @@ const Start = () => {
           width="72"
           height="72"
         />
-        <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+        <h1 className="display-5 fw-bold text-body-emphasis font-custom-color">
+          InstarResume
+        </h1>
         <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
+          <p className="lead mb-4 font-custom-color">
+            Elevate your career with "InstarResume", the ultimate resume-making
+            solution designed to empower you on your professional journey. Craft
+            visually stunning resumes effortlessly, and showcase your skills and
+            experiences in a way that stands out.
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
-              Primary button
+              Login
             </button>
             <button
               type="button"
               className="btn btn-outline-secondary btn-lg px-4"
             >
-              Secondary
+              Register
             </button>
           </div>
         </div>
