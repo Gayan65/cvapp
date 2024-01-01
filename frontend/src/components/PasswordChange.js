6622319cab243a86afbe9316eb8d6ff68d980158
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-01 06:35:45 +0200

    Css model add to the password change component, working

--- frontend/src/components/PasswordChange.js
+++ frontend/src/components/PasswordChange.js
@@ -88,9 +88,50 @@ const PasswordChange = (props) => {
                   </div>
                 </div>
 
-                <button className="btn btn-danger btn" type="submit">
+                <button
+                  className="btn btn-danger btn"
+                  type="submit"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
                   Change
                 </button>
+
+                <div
+                  className="modal fade"
+                  id="exampleModal"
+                  tabIndex="-1"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="exampleModalLabel">
+                          Password Change Status
+                        </h1>
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div className="modal-body">
+                        Password successfully Changed !
+                      </div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Okay
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </form>
               {message && (
                 <div className="alert alert-primary mt-3" role="alert">
