771e40f2e6e0aafb4dedfe704ca9875db5ae450c
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-03 16:56:55 +0200

    Personal_info.js protected route add, working

--- frontend/src/components/cv_components/Personal_Info.js
+++ frontend/src/components/cv_components/Personal_Info.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useNavigate} from "react-router-dom"
 
 const Personal_Info = () => {
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+
+  //protecting this route
+  useEffect(() => {
+    if(token === null || token ==="") {
+      navigate('/login')
+    }
+    else{
+      console.log("You are good to go!")
+    }
+    // eslint-disable-next-line
+  }, [])
   return <div>Personal_Info</div>;
 };
 
