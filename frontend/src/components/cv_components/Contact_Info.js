7d098b71eec08d2399bf52c3d6acc9c52ae85c92
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-06 14:33:24 +0200

    protect contact_info.js route, working

--- frontend/src/components/cv_components/Contact_Info.js
+++ frontend/src/components/cv_components/Contact_Info.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact_Info = () => {
+  const token = sessionStorage.getItem("token");
+  const navigate = useNavigate();
+
+  //protecting this route
+  useEffect(() => {
+    if (token === null || token === "") {
+      navigate("/login");
+    } else {
+      console.log("You are fine!");
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return <div>Contact_Info</div>;
 };
 
