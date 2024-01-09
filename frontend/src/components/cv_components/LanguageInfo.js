08dae667d4cf4a9fa8307fee2d1be816ed43bd14
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-09 21:47:39 +0200

    LanguageInfo.js protected route, working

--- frontend/src/components/cv_components/LanguageInfo.js
+++ frontend/src/components/cv_components/LanguageInfo.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LanguageInfo = () => {
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+
+  //Protecting route
+  useEffect(() => {
+    if (token === null || token === "") {
+      navigate("/login");
+    } else {
+      console.log("Good to go");
+    }
+    // eslint-disable-next-line
+  }, []);
+
   return <div>LanguageInfo</div>;
 };
 
