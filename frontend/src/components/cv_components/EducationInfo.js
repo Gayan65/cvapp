9817b742b93ea26bd530704abb8dc44bf00cb9e0
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-11 20:52:28 +0200

    EducationInfo route protect, working

--- frontend/src/components/cv_components/EducationInfo.js
+++ frontend/src/components/cv_components/EducationInfo.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const EducationInfo = () => {
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+
+  //protecting this route
+  useEffect(() => {
+    if (token === null || token === "") {
+      navigate("/login");
+    } else {
+      console.log("Good to go!");
+    }
+    // eslint-disable-next-line
+  }, []);
   return <div>EducationInfo</div>;
 };
 
