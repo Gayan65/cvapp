54fa2c2984f8a3f484d0baafef3fdad4a58b6012
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-14 17:41:35 +0200

    Navbar add css

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import navLogo from "../images/logos/Logo.png";
 import NavUser from "./NavUser";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserLock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faHouse,
+  faMicrochip,
+  faBuilding,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -21,7 +26,7 @@ const NavBar = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand me-5 " href="/">
+          <a className="navbar-brand me-5" href="/">
             <span className="custom-nav-logo-instar fs-3 fw-bold ">Instar</span>
             <span className="custom-nav-logo-resume fs-3 fw-bold ">Resume</span>
           </a>
@@ -40,30 +45,34 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="/home">
-                  Home
+                <a className="nav-link fs-5" href="/home">
+                  <FontAwesomeIcon icon={faHouse} />
+                  <span className="ms-1">Home</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About Us
+                <a className="nav-link fs-5 " href="/">
+                  <FontAwesomeIcon icon={faBuilding} />
+                  <span className="ms-1">About Us</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Product
+                <a className="nav-link fs-5" href="/">
+                  <FontAwesomeIcon icon={faMicrochip} />
+                  <span className="ms-1">Product</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Search People
+                <a className="nav-link fs-5 " href="/">
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  <span className="ms-1">Search</span>
                 </a>
               </li>
             </ul>
 
             {isLoggedIn === false ? (
-              <a className="btn btn-outline-secondary" href="/login">
-                <FontAwesomeIcon icon={faUserLock} />
+              <a className="btn btn-outline-secondary fs-5 " href="/login">
+                <FontAwesomeIcon icon={faUser} />
                 <span className="ms-2">Login</span>
               </a>
             ) : (
