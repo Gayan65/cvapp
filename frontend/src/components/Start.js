37743eea9fd4f2a2cc2e9688db1f60f1775b52e3
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-01-19 13:46:57 +0200

    Startpage heros section css modified

--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -1,44 +1,45 @@
 import React from "react";
-import smallLoago from "../images/logos/LogoSmall.png";
+import headingImg from "../images/assetsImg/professional.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faUserPen } from "@fortawesome/free-solid-svg-icons";
 
 const Start = () => {
   return (
     <div>
-      <div className="px-4 py-5 my-5 text-center">
-        <img
-          className="d-block mx-auto mb-4"
-          src={smallLoago}
-          alt=""
-          width="85"
-          height="85"
-        />
-        <h1 className="display-5 fw-bold text-body-emphasis font-custom-color">
-          InstarResume
-        </h1>
-        <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4 font-custom-color">
-            Elevate your career with "InstarResume", the ultimate resume-making
-            solution designed to empower you on your professional journey. Craft
-            visually stunning resumes effortlessly, and showcase your skills and
-            experiences in a way that stands out.
-          </p>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <a
-              className="btn btn-primary btn-lg px-4 gap-3 btn-custom-animation-n-y"
-              href="/login"
-            >
-              <FontAwesomeIcon icon={faUser} />
-              <span className=" ms-3 ">Login</span>
-            </a>
-            <a
-              className="btn btn-outline-secondary btn-lg px-4 btn-custom-animation-p-y"
-              href="/register"
-            >
-              <FontAwesomeIcon icon={faUserPen} />
-              <span className="ms-2">Register</span>
-            </a>
+      <div className="container col-xxl-8 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-7">
+            <img
+              src={headingImg}
+              className="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="745"
+              height="559"
+              loading="lazy"
+            />
+          </div>
+          <div className="col-lg-5">
+            <h1 className="display-5 fw-bold text-body-emphasis increased-line-spacing-heading mb-5 text-end">
+              "Elevate Your Professional Profile with isntarResume”
+            </h1>
+            <p className="lead increased-line-spacing-subheading fs-4 text-end mb-4">
+              Designed to effortlessly showcase your skills and experience in
+              the best light possible.
+            </p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+              <a
+                className="btn btn-primary btn-lg px-4 gap-3 btn-custom-animation-n-y custom-btn-shadow "
+                href="/login"
+              >
+                <span className=" mx-4 fs-4  ">Login</span>
+              </a>
+              <a
+                className="btn btn-outline-secondary btn-lg px-4 btn-custom-animation-p-y custom-btn-shadow"
+                href="/register"
+              >
+                <span className="mx-3 fs-4 ">Sign Up</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
