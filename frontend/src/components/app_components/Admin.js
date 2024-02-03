1312f0173bde0fdefceb74ea88f9857b7aab1999
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-02-03 10:56:17 +0200

    users renders in fron end

--- frontend/src/components/app_components/Admin.js
+++ frontend/src/components/app_components/Admin.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+  // Setting users
+  const [users, setUsers] = useState([]);
+
+  //Making the header
+  const headers = {
+    Authorization: `bearer ${token}`,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  //Checking user has logged
+  useEffect(() => {
+    if (token === "" || token === null) {
+      navigate("/login");
+    } else {
+      axios
+        .get(`http://localhost:4000/api/user/all`, { headers })
+        .then((response) => {
+          console.log(response.data);
+          setUsers(response.data);
+        })
+        .catch((error) => {
+          // Handle errors
+          //once the token expires user redirect to the Error page
+          if (error.response.status === 403) navigate("/login");
+          else console.error("Error fetching user data:", error);
+        });
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <main className="form-signin w-100 m-auto custom-border-login">
+          {users.map((user, i) => (
+            <div key={i} className=" fs-2 p-3 me-4">
+              {user.email} <button className=" btn btn-danger ">Delete</button>
+            </div>
+          ))}
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
